481b7e6b259233060ea3ca779cb65eed7bafa1b8
Fardin <webFardin@gmail.com>
2023-01-06 17:58:03 +0330

    add actions-section and some changes in ui 01-shufflify

--- 01-Shufflify/script.js
+++ 01-Shufflify/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const clearInputIcons = document.querySelectorAll('.clear-input-icon');
+
+clearInputIcons.forEach((icon) => icon.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  const input =
+  document.querySelector(`input#${e.currentTarget.dataset.input}`);
+  input.value = '';
+  input.focus();
+}));
