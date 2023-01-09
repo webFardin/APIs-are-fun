cebe20fdb0aace0b10c9477d27ec0f14e3a66791
Fardin <webFardin@gmail.com>
2023-01-09 20:51:35 +0330

    add functionality to actions-section 01-shufflify

--- 01-Shufflify/script.js
+++ 01-Shufflify/script.js
@@ -4,12 +4,19 @@
 const clearInputIcons = document.querySelectorAll('.clear-input-icon');
 
 const artistInputField = document.getElementById('artistInput');
+const genreInputField = document.getElementById('genreInput');
+const yearsInputField = document.getElementById('yearsInput');
 
 const artistsSearchListWrapper =
-document.getElementById('artistsSearchListWrapper');
+  document.getElementById('artistsSearchListWrapper');
 
 const artistsSearchItems =
-document.getElementsByClassName('artists-search-item');
+  document.getElementsByClassName('artists-search-item');
+
+const guitarAnimationItems =
+  document.querySelectorAll('.guitar-animation-item');
+
+const shuffleButton = document.getElementById('shuffleButton');
 
 clearInputIcons.forEach((icon) => icon.addEventListener('mousedown', (e) => {
   e.preventDefault();
@@ -26,7 +33,7 @@ clearInputIcons.forEach((icon) => icon.addEventListener('mousedown', (e) => {
 const encodedBody = new URLSearchParams();
 encodedBody.append('grant_type', 'client_credentials');
 
-const getTokenRequestOptions = {
+const getTokenReqOptions = {
   method: 'POST',
   headers: {
     'Authorization':
@@ -38,9 +45,9 @@ const getTokenRequestOptions = {
 };
 
 let token;
-async function tokenRequest() {
+async function getTokenReq() {
   const tokenReqObj = await fetch('https://accounts.spotify.com/api/token',
-      getTokenRequestOptions);
+      getTokenReqOptions);
 
   const tokenReqRes = await tokenReqObj.json();
 
@@ -49,7 +56,7 @@ async function tokenRequest() {
   // when token expired
   setTimeout(() => {
     token = null;
-    tokenRequest();
+    getTokenReq();
   }, 3500000);
 
   // remove this later
@@ -57,9 +64,9 @@ async function tokenRequest() {
 
   return token;
 }
-tokenRequest();
+getTokenReq();
 
-// tokenRequest()
+// getTokenReq()
 // // customize catch later...
 //     .catch((error) => console.log('error in getting token', error));
 // ;
@@ -140,4 +147,146 @@ function artistsSearchListShower(artistsArr) {
       artistSearchInputFieldEvent();
     });
   });
+}
+
+function animateGuitarButton(cancel) {
+  if (cancel) {
+    guitarAnimationItems.forEach((item) =>{
+      item.getAnimations().forEach((item) => {
+        item.cancel();
+      });
+    });
+    return;
+  };
+
+  guitarAnimationItems.forEach((item, index) => {
+    item.animate(
+        [
+          {opacity: 0.3},
+          {opacity: 1, offset: 0.6},
+          {opacity: 1},
+        ], {
+          duration: 1500,
+          delay: index * 350,
+          easing: 'ease-in-out',
+          iterations: Infinity,
+          direction: 'alternate-reverse',
+        },
+    );
+  });
+}
+
+shuffleButton.addEventListener('click', getItemsReq);
+
+let getItemsReqIsProcessing = false;
+async function getItemsReq() {
+  // don't make extra requests
+  if (getItemsReqIsProcessing) return;
+  getItemsReqIsProcessing = true;
+
+  animateGuitarButton();
+
+  const query = getItemsReqQueryMaker();
+
+  const OffsetNum = await randomOffsetNumGenReq(query);
+
+  const getItemsReqObj = await fetch(`https://api.spotify.com/v1/search?${query}&type=track&include_external=audio&limit=50&offset=${OffsetNum}`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  const getItemsReqRes = await getItemsReqObj.json();
+
+  const getItemsReqResLength = getItemsReqRes.tracks.items.length;
+  const randomNumber = Math.floor(Math.random() * getItemsReqResLength);
+
+  const randomSelectedItem = getItemsReqRes.tracks.items[randomNumber];
+  
+  const trackName = randomSelectedItem.name;
+  const trackDemo = randomSelectedItem.preview_url;
+  const trackLink = randomSelectedItem.external_urls.spotify;
+  const trackAlbumName = randomSelectedItem.album.name;
+  const trackAlbumLink = randomSelectedItem.album.external_urls.spotify;
+  // get cover based on screen resolution
+  // const trackCover = randomSelectedItem.album[2].url;
+  // iterate over artists
+  const trackArtists = randomSelectedItem.artists[0].name;
+  const trackArtistsLinks = randomSelectedItem.artists[0].external_urls.spotify;
+
+  animateGuitarButton(true);
+  getItemsReqIsProcessing = false;
+
+  console.log(getItemsReqRes);
+
+  return randomSelectedItem;
+}
+
+async function randomOffsetNumGenReq(query) {
+  const totalItemsNumReqObj = await fetch(`https://api.spotify.com/v1/search?${query}&type=track&include_external=audio&limit=50`, {
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  const totalItemsNumReqRes = await totalItemsNumReqObj.json();
+
+  const totalItemsNum = totalItemsNumReqRes.tracks.total;
+
+  // offset > 950 lead to error
+  const randomOffsetNum = totalItemsNum >= 950 ?
+  Math.floor(950 * Math.random()) :
+  Math.floor(totalItemsNum * Math.random());
+
+  return randomOffsetNum;
+}
+
+function standardizeInputFields() {
+  // artist input field
+  const standardizedArtistInputField = artistInputFieldStandardize();
+
+  // genre input field
+  const trimmedGenreInputField = genreInputField.value.trim();
+  const standardizedGenreInputField =
+    trimmedGenreInputField.replaceAll(' ', '-');
+
+  // years input field
+  const trimmedYearsInputField = yearsInputField.value.trim();
+  const standardizedYearsInputField =
+    trimmedYearsInputField.replaceAll(' ', '-');
+
+  return [standardizedArtistInputField,
+    standardizedGenreInputField,
+    standardizedYearsInputField];
+}
+
+function getItemsReqQueryMaker() {
+  let [artist, genre, years] = standardizeInputFields();
+
+  // empty filters or extra spaces have effect on results!
+  artist = artist ? 'artist:' + artist + '%20' : '';
+  genre = genre ? 'genre:' + genre + '%20' : '';
+  years = years ? 'year:' + years : '';
+
+  let randomStr = '';
+  // randomStr when artist field is filled, doesn't make good responses
+  if (artist == '') {
+    randomStr = randomStrGenerator(3) + '%20';
+  }
+
+  const query = `q=${randomStr}${artist}${genre}${years}`;
+
+  return query;
+}
+
+function randomStrGenerator(length) {
+  let randomString = '';
+
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
+  for ( let i = 0; i < length; i++ ) {
+    const randomIndex = characters.length * Math.random();
+    randomString += characters.charAt(Math.floor(randomIndex));
+  }
+
+  return randomString;
 }
